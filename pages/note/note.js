e6bffac9068cb016de3bfd185a97472441e54e44
ouzhibing <ouzhibing@galaxymx.com>
2018-09-26 17:57:07 +0800

    查看笔记详情

--- pages/note/note.js
+++ pages/note/note.js
@@ -37,5 +37,15 @@ Page({
       }
       
     })
-  }
+  },
+  /**
+ * 进入专辑详情页面
+ */
+  openDetail: function (e) {
+    let id = e.currentTarget.dataset.id;
+
+    wx.navigateTo({
+      url: '/pages/article/article?id=' + id
+    })
+  },
 })
